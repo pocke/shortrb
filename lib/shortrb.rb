5236b2cd4c18821f67cc97c77c8106807e859f0e
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-04-23 20:59:24 +0900

    WIP implementation

--- lib/shortrb.rb
+++ lib/shortrb.rb
@@ -1,6 +1,130 @@
 require "shortrb/version"
 
+using(Module.new do
+  refine RubyVM::AbstractSyntaxTree::Node do
+    def [](idx)
+      children[idx]
+    end
+  end
+end)
+
 module Shortrb
-  class Error < StandardError; end
-  # Your code goes here...
+  class AstToStr
+    def self.convert(root)
+      self.new(root).convert
+    end
+
+    def initialize(root)
+      @root = root
+      @stack = []
+    end
+
+    def convert
+      _convert(@root)
+    end
+
+    private def _convert(node)
+      @stack.push node
+      res = __send__(:"on_#{node.type}", node)
+      @stack.pop
+      res
+    end
+
+    private def parent
+      @stack[-2]
+    end
+
+    private def on_SCOPE(node)
+      _convert node[2]
+    end
+
+    private def on_CLASS(node)
+      "class #{_convert node[0]};#{_convert node[2]};end"
+    end
+
+    private def on_COLON2(node)
+      parent, this = node.children
+      if parent
+        "#{_convert parent}::#{this}"
+      else
+        this.to_s
+      end
+    end
+
+    private def on_BEGIN(node)
+      # XXX: is it ok?
+      ""
+    end
+
+    private def on_BLOCK(node)
+      node.children
+        .map { |child| _convert(child) }
+        .reject { |str| str.empty? }
+        .join(';')
+    end
+
+    private def on_VCALL(node)
+      node[0].to_s
+    end
+
+    private def on_FCALL(node)
+      method_name = node[0]
+      args = _convert node[1]
+      if space_necessary_between_method_name_and_args?(method_name.to_s, args)
+        "#{method_name} #{args}"
+      else
+        "#{method_name}#{args}"
+      end
+    end
+
+    private def on_CALL(node)
+      method_name = +"#{_convert node[0]}.#{node[1]}"
+      return method_name unless node[2]
+      args = _convert(node[2])
+      if space_necessary_between_method_name_and_args?(method_name.to_s, args)
+        "#{method_name} #{args}"
+      else
+        "#{method_name}#{args}"
+      end
+    end
+
+    private def on_ARRAY(node)
+      res = node.children.compact.map { |child| _convert child }.join(',')
+      if parent.type == :FCALL || parent.type == :CALL
+        res
+      else
+        "[#{res}]"
+      end
+    end
+
+    private def on_LIT(node)
+      node.children.first.inspect
+    end
+
+    private def on_STR(node)
+      node.children.first.inspect
+    end
+
+    private def on_LASGN(node)
+      "#{node[0]}=#{_convert node[1]}"
+    end
+
+    alias on_GASGN on_LASGN
+    alias on_IASGN on_LASGN
+    alias on_CVASGN on_LASGN
+
+    private def on_LVAR(node)
+      "#{node[0]}"
+    end
+
+    alias on_GVAR on_LVAR
+    alias on_IVAR on_LVAR
+    alias on_CVAR on_LVAR
+
+    private def space_necessary_between_method_name_and_args?(method_name, args)
+      # TODO: is it right?
+      return false if method_name.end_with?('?', '!')
+      return args.match?(/\A[[:alnum:]]/)
+    end
+  end
 end
---
+++ test/shortrb_test.rb
@@ -0,0 +1,113 @@
+require 'shortrb'
+require 'minitest/autorun'
+
+class AstToStrTest < Minitest::Test
+
+  # classes
+
+  def test_class
+    # TODO: it should be "class A;end"
+    assert_ast_to_str "class A;;end", <<~RUBY
+      class A
+      end
+    RUBY
+  end
+
+  def test_class_with_content
+    assert_ast_to_str "class A;x;y;end", <<~RUBY
+      class A
+        x
+        y
+      end
+    RUBY
+  end
+
+  # method calls
+
+  def test_vcall
+    assert_ast_to_str "x", "x"
+  end
+
+  def test_fcall
+    assert_ast_to_str "x y", "x y"
+    assert_ast_to_str "x y,z", "x y, z"
+    assert_ast_to_str "x y", "x(y)"
+    assert_ast_to_str "x@y", "x(@y)"
+  end
+
+  def test_call
+    assert_ast_to_str "x.y", "x.y"
+    assert_ast_to_str "x.y z", "x.y(z)"
+    assert_ast_to_str "x.y@z", "x.y(@z)"
+  end
+
+  # operators
+
+  # literals
+
+  def test_array
+    assert_ast_to_str "[x]", "[ x ]"
+    assert_ast_to_str "[x,y]", "[ x, y ]"
+    assert_ast_to_str "[x,y,z]", "[x, y, z]"
+  end
+
+  def test_int
+    assert_ast_to_str "1", "1"
+  end
+
+  def test_float
+    assert_ast_to_str "1.1", "1.1"
+  end
+
+  def test_string
+    assert_ast_to_str '"foo"', "'foo'"
+    assert_ast_to_str '"foo"', '"foo"'
+  end
+
+  def test_symbol
+    assert_ast_to_str ':foo', ':foo'
+  end
+
+  # assigns
+
+  def test_lasgn
+    assert_ast_to_str 'a=1', 'a = 1'
+  end
+
+  def test_gasgn
+    assert_ast_to_str '$a=1', '$a = 1'
+  end
+
+  def test_iasgn
+    assert_ast_to_str '@a=1', '@a = 1'
+  end
+
+  def test_cvasgn
+    assert_ast_to_str '@@a=1', '@@a = 1'
+  end
+
+  # variable references
+
+  def test_lvar
+    assert_ast_to_str 'a=1;p a', 'a = 1; p a'
+  end
+
+  def test_gvar
+    assert_ast_to_str '$a=1;p$a', '$a = 1; p $a'
+  end
+
+  def test_ivar
+    assert_ast_to_str '@a=1;p@a', '@a = 1; p @a'
+  end
+
+  def test_cvar
+    assert_ast_to_str '@@a=1;p@@a', '@@a = 1; p @@a'
+  end
+
+  # helpers
+
+  def assert_ast_to_str(expected, original)
+    root = RubyVM::AbstractSyntaxTree.parse(original)
+    assert_equal expected, Shortrb::AstToStr.convert(root)
+  end
+end
